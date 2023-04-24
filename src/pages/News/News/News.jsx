83dd27447eceb9377ab65bdd300416c1368811e1
ehmunna <emran-hussain.munna-tu-ilmenau.de>
2023-04-24 14:23:09 +0200

    63-5 Display News Details and editors insights

--- src/pages/News/News/News.jsx
+++ src/pages/News/News/News.jsx
@@ -1,9 +1,40 @@
 import React from 'react';
+import { Link, useLoaderData } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import { FaArrowLeft } from 'react-icons/fa';
+import EditorsInsights from './EditorsInsights';
 
 const News = () => {
+  const loadedNews = useLoaderData();
+  const { thumbnail_url, title, details, category_id } = loadedNews;
+  console.log(loadedNews);
   return (
     <div>
-      <h3>News...</h3>
+      <div>
+        <Card>
+          <Card.Img variant="top" src={thumbnail_url} />
+          <Card.Body>
+            <Card.Title>{title}</Card.Title>
+            <Card.Text>{details}</Card.Text>
+            <Link to={`/categories/${category_id}`}>
+              <Button
+                className="inline-flex align-items-center"
+                variant="danger rounded-0"
+              >
+                {' '}
+                <span className="me-2">
+                  <FaArrowLeft></FaArrowLeft>
+                </span>
+                <span>All news in this category</span>
+              </Button>
+            </Link>
+          </Card.Body>
+        </Card>
+      </div>
+      <div>
+        <EditorsInsights></EditorsInsights>
+      </div>
     </div>
   );
 };
